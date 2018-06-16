8cb626fad1763fb9f83d97f0eaf034934b335d78
Yefim Vedernikoff <yefim323@gmail.com>
2018-06-16 00:00:14 -0700

    Resume on Close

--- src/have-i-downloaded-this-before.js
+++ src/have-i-downloaded-this-before.js
@@ -1,23 +1,32 @@
 var debug = localStorage.getItem('debug') === 'true';
+var noop = function() {};
+var id = {
+  serialize: function(downloadId) {
+    return ['hidbt', downloadId].join('|||');
+  },
+  parse: function(notificationId) {
+    return parseInt(notificationId.split('|||').pop(), 10);
+  }
+};
 
 debug && console.log('Binding event handlers...');
 
+// Once a user starts downloading a file
 chrome.downloads.onCreated.addListener(function(item) {
-  console.log(item);
-
-  debug && console.log(item.id);
-  debug && console.log(item.exists);
-
+  // Pause the download
   chrome.downloads.pause(item.id, function() {
+    // Check if the download already exists
     chrome.downloads.search({url: item.url, state: 'complete', exists: true}, function(items) {
       if (items.length) {
-        chrome.notifications.create(['hidtb', items[0].id].join('|||'), {
+        // If it exists, prompt the user to open
+        chrome.notifications.create(id.serialize(items[0].id), {
           type: 'basic',
           title: 'You\'ve downloaded this before!',
           iconUrl: '../icon128.png',
-          message: 'Click to open file'
-        }, function() {});
+          message: 'Click to open file. Close to resume download.'
+        }, noop);
       } else {
+        // Otherwise, resume the new download
         chrome.downloads.resume(item.id);
       }
     });
@@ -27,6 +36,11 @@ chrome.downloads.onCreated.addListener(function(item) {
 chrome.notifications.onClicked.addListener(function(notificationId) {
   debug && console.log(notificationId);
 
-  var id = parseInt(notificationId.split('|||').pop(), 10);
-  chrome.downloads.open(id);
+  chrome.downloads.open(id.parse(notificationId));
+  chrome.notifications.clear(notificationId, noop);
+});
+
+chrome.notifications.onClosed.addListener(function(notificationId, byUser) {
+  debug && console.log('byUser:', byUser);
+  chrome.downloads.resume(id.parse(notificationId));
 });
