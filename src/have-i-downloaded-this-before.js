cfaebd6e7b825eb17f14912ca03ccc34e664c12c
Yefim Vedernikoff <yefim323@gmail.com>
2018-06-15 23:39:52 -0700

    Add search

--- src/have-i-downloaded-this-before.js
+++ src/have-i-downloaded-this-before.js
@@ -1,20 +1,32 @@
+var debug = localStorage.getItem('debug') === true
+
+debug && console.log('Binding event handlers...');
+
 chrome.downloads.onCreated.addListener(function(item) {
-  if (item.exists) {
-    chrome.downloads.pause(item.id, function() {
-      chrome.notifications.create('hidtb', {
-        type: 'basic',
-        title: 'You\'ve downloaded this before!',
-        iconUrl: '../icon128.png',
-        message: 'Do you want to open it?',
-        buttons: [
-          {title: 'Open'},
-          {title: 'Resume'}
-        ]
-      }, function() {});
+  console.log(item);
+
+  debug && console.log(item.id);
+  debug && console.log(item.exists);
+
+  chrome.downloads.pause(item.id, function() {
+    chrome.downloads.search({url item.url, exists: true}, function(items) {
+      if (items.length) {
+        chrome.notifications.create(['hidtb', items[0].id].join('|||'), {
+          type: 'basic',
+          title: 'You\'ve downloaded this before!',
+          iconUrl: '../icon128.png',
+          message: 'Click to open file'
+        }, function() {});
+      } else {
+        chrome.downloads.resume(item.id);
+      }
     });
-  }
+  });
 });
 
-chrome.notifications.onButtonClicked.addListener(function(notificationId, buttonIndex) {
-  console.log(buttonIndex);
+chrome.notifications.onClicked.addListener(function(notificationId) {
+  debug && console.log(notificationId);
+
+  var id = parseInt(notificationId.split('|||').pop(), 10);
+  chrome.downloads.open(id);
 });
